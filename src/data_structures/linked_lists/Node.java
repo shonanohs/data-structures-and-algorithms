28a6bb2104d568742306465fbea576bbad03d9d6
Shona Fitzpatrick <71610718+shonanohs@users.noreply.github.com>
2025-10-18 12:39:29 +0100

    LinkedList implementation

---
+++ src/data_structures/linked_lists/Node.java
@@ -0,0 +1,23 @@
+package data_structures.linked_lists;
+
+public class Node {
+    private int value;
+
+    private Node next;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
