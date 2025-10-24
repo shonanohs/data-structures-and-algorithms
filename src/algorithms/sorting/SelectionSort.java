ec0ef8c871942661dc303b854a33e5fc2389a7ee
shonanohs <shonafitzpatrick15@gmail.com>
2025-10-24 13:21:15 +0100

    Added SelectionSort implementation

---
+++ src/algorithms/sorting/SelectionSort.java
@@ -0,0 +1,37 @@
+package algorithms.sorting;
+
+public class SelectionSort {
+
+    // Time complexity: O(n²)
+    // Space complexity: O(1)
+    public static int[] selectionSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+        return array;
+    }
+
+    public static void printArray(int[] array) {
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 0; i < array.length; i++) {
+            sb.append(array[i]);
+            if (i < array.length - 1) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) {
+        printArray(selectionSort(new int[]{8, 3, 1, 2, 11, 19, 4, 6}));
+    }
+}
